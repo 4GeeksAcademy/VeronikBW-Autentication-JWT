92cb708fec01067311e658dbc37eee043e3493b7
Verónica Rodríguez <veronikbw@gmail.com>
2025-11-03 17:05:21 +0000

    Added sign up, salt, and hashed password

--- src/api/models.py
+++ src/api/models.py
@@ -8,12 +8,14 @@ class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    salt: Mapped[str] = mapped_column(String(180), nullable=False)
+
+    def __repr__(self):
+        return f"<User {self.email}>"
 
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,10 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+import os
+from base64 import b64encode
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
 
 api = Blueprint('api', __name__)
 
@@ -12,11 +16,42 @@ api = Blueprint('api', __name__)
 CORS(api)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/health-check', methods=['GET'])
+def health_check():
+    return jsonify({"status": "OK"}), 200
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+
+@api.route('/signup', methods=['POST'])
+def signup_user():
+    data = request.json
+    data = {
+        "email": data.get("email"),
+        "password": data.get("password"),
+        "salt": 1
     }
 
-    return jsonify(response_body), 200
+    if not data["email"] or not data["password"]:
+        return jsonify({"message": "Email and password are required"}), 400
+    
+    if User.query.filter_by(email=data["email"]).first():
+        return jsonify({"message": "User already exists"}), 409
+    
+    salt = b64encode(os.urandom(32)).decode('utf-8')
+    password = generate_password_hash(f"{data['password']}{salt}")
+
+    new_user = User(
+        email=data["email"],
+        password=password,
+        salt=salt   
+    )
+
+    db.session.add(new_user)
+
+    try:
+        db.session.commit()
+        return jsonify({"message": "User created successfully"}), 201
+    
+    except Exception as error:
+        db.session.rollback()
+        return jsonify({"message": "Error creating user", "error": str(error)}), 500
+    
